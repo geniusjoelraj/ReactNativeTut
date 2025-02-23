8e867f803e4a3419a55d6ab2af9d10b67a14ead2
geniusjoelraj <geniusjoel2005@gmail.com>
2025-02-23 18:32:46 +0530

    Setup Nativewind

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 import { Link } from "expo-router";
-import '../global.css';
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-content bg-white">
-      <Text>Hello World!</Text>
+    <View 
+      className="flex-1 items-center justify-center bg-red">
+      <Text className="text-3xl">Hello World!</Text>
       <StatusBar style="auto"/>
       <Link style={{color: 'blue'}} href="/profile">Go to profile</Link>
     </View>
