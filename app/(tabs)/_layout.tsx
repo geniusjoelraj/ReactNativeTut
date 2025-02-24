f3e00f1fcce66184d5f491dd1b1195211bf14866
geniusjoelraj <geniusjoel2005@gmail.com>
2025-02-25 01:00:49 +0530

    cleaned up tab nav bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -9,14 +9,19 @@ type TabIconProp = {
 };
 const TabIcon = ({ icon, color, name, focused }: TabIconProp) => {
   return (
-    <View className='justify-center gap-2' >
+    <View className='flex justify-center items-center gap-2 h-24 w-24 pt-5' >
       <Image
         source={icon}
         resizeMode='contain'
         tintColor={color}
         className='h-6 w-6'
       />
-      <Text className={`${focused ? 'font-psemibold' : 'font-pregular'} text-xs self-center text-nowrap`}>{name}</Text>
+      <Text
+        className={`${focused ? 'font-pbold' : 'font-pregular'} text-xs text-center`}
+        style={{ color: color }}
+      >
+        {name}
+      </Text>
     </View>
 
   )
@@ -25,9 +30,16 @@ const TabsLayout = () => (
   <>
     <Tabs
       screenOptions={{
-        tabBarShowLabel: false
-      }}
-    >
+        tabBarActiveTintColor: "#FFA001",
+        tabBarInactiveTintColor: "#CDCDE0",
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: "#161622",
+          borderTopWidth: 1,
+          borderTopColor: "#232533",
+          height: 84,
+        },
+      }}   >
       <Tabs.Screen
         name='home'
         options={{
@@ -85,7 +97,6 @@ const TabsLayout = () => (
           )
         }} />
     </Tabs>
-    )
   </>
 )
 
