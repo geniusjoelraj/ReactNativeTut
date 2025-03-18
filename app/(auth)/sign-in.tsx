7bc1476ab4d5c391a5b0ebcb909e1349dfa58ccd
geniusjoelraj <geniusjoel2005@gmail.com>
2025-03-18 15:58:15 +0530

    Auth login page

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,13 +1,18 @@
 import { View, Text, SafeAreaView, ScrollView, Image } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { images } from '@/constants'
+import FormField from '../Components/FornField'
 
 const SignIn = () => {
+  const [form, setForm] = useState({
+    email: '',
+    password: ''
+  });
   return (
     <>
       <SafeAreaView className='bg-primary h-full'>
         <ScrollView>
-          <View className='w-full items-center justify-center px-4 my-6 h-3/4'>
+          <View className='w-full px-4 my-16 h-full'>
             <Image
               source={images.logo}
               resizeMode='contain'
@@ -16,6 +21,28 @@ const SignIn = () => {
             <Text className='text-2xl text-white mt-10 font-pbold'>
               Log in to Aora
             </Text>
+            <FormField
+              title="Email"
+              value={form.email}
+              handleChangeText={(e: any) => setForm({
+                ...form,
+                email: e
+              })}
+              otherStyles="mt-8"
+              keyboardType="email-address"
+
+            />
+            <FormField
+              title="password"
+              value={form.password}
+              handleChangeText={(e: any) => setForm({
+                ...form,
+                password: e
+              })}
+              otherStyles="mt-8"
+            // keyboardType="email-address"
+
+            />
           </View>
         </ScrollView>
       </SafeAreaView>
